e311303396bd67eb86b08f737938499d8afa6499
edunbach <143748179+edunbach@users.noreply.github.com>
2023-10-09 18:15:07 -0700

    Add files via upload

--- ladder.py
+++ ladder.py
@@ -4,14 +4,14 @@ def my_steps(x):
     if x < 1 or x > 25:
         raise ValueError("The input must be an ingteger between 1 and 25")
     
-        output = [0] * (x+1)
+    output = [0] * (x+1)
 
-        output[1] = 1
-        output[2] = 2
+    output[1] = 1
+    output[2] = 2
 
-        for i in range (3, x+1):
-            output[i] = output[i-1] + output[i-2]
+    for i in range (3, x+1):
+        output[i] = output[i-1] + output[i-2]
 
-        return output[x]
+    return output[x]
 
 
