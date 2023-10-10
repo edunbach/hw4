0971c363322480f6a69aa3aef3aea3887add304f
edunbach <143748179+edunbach@users.noreply.github.com>
2023-10-09 18:19:02 -0700

    Add files via upload

--- bug1.py
+++ bug1.py
@@ -28,38 +28,3 @@ class Circle(Base):
 
 '''
 
-def main():
-    c = Circle(1, 2, 3)
-    print(c.draw())
-
-if __name__=="__main__":
-    main()
-
-class Square(Base):
-    def __init__(self, x, y, size):
-        super().__init__(x, y, size)
-
-    def draw(self):
-        return f'''This is a circle
-({self.x}, {self.y})
-{self.size}
-         , - ~ ~ ~ - ,
-     , '               ' ,
-   ,                       ,
-  ,                         ,
- ,                           ,
- ,                           ,
- ,                           ,
-  ,                         ,
-   ,                       ,
-     ,                  , '
-       ' - , _ _ _ ,  '
-
-'''
-
-def main():
-    c = Square(1, 2, 3)
-    print(c.draw())
-
-if __name__=="__main__":
-    main()
